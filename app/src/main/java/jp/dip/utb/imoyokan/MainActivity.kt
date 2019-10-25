dc8ce29852389d44550484df12fe24773c40157c
utubo <utbutb@gmail.com>
2019-10-26 03:44:12 +0900

    あぁそう書けるんだ…

--- app/src/main/java/jp/dip/utb/imoyokan/MainActivity.kt
+++ app/src/main/java/jp/dip/utb/imoyokan/MainActivity.kt
@@ -4,8 +4,8 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import java.lang.Thread.sleep
@@ -14,17 +14,11 @@ import java.lang.Thread.sleep
 class MainActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceChangeListener {
 
     private lateinit var pref: Pref
-    private lateinit var topMessage: TextView
-    private lateinit var catalog: TextView
-    private lateinit var thread: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         pref = Pref.getInstance(applicationContext)
-        topMessage = findViewById(R.id.message)
-        catalog = findViewById(R.id.catalog)
-        thread = findViewById(R.id.thread)
         refresh()
     }
 
@@ -46,9 +40,9 @@ class MainActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceCh
     }
 
     private fun refresh() {
-        topMessage.visibility = visibleOrGone(pref.lastCatalogUrl.isEmpty() && pref.lastThreadUrl.isEmpty())
-        catalog.visibility = visibleOrGone(pref.lastCatalogUrl.isNotEmpty())
-        thread.visibility = visibleOrGone(pref.lastThreadUrl.isNotEmpty())
+        top_message.visibility = visibleOrGone(pref.lastCatalogUrl.isEmpty() && pref.lastThreadUrl.isEmpty())
+        catalog_button.visibility = visibleOrGone(pref.lastCatalogUrl.isNotEmpty())
+        thread_button.visibility = visibleOrGone(pref.lastThreadUrl.isNotEmpty())
     }
 
     private fun autoFinish(delayMills: Long = 200L) {
