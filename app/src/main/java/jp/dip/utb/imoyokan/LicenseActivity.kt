64f2dfde025b86999fefc77456b37221be9cede2
utubo <utbutb@gmail.com>
2019-10-27 12:04:39 +0900

    ライセンス表記

---
+++ app/src/main/java/jp/dip/utb/imoyokan/LicenseActivity.kt
@@ -0,0 +1,26 @@
+package jp.dip.utb.imoyokan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+
+class LicenseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_license)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            // Respond to the action bar's Up/Home button
+            android.R.id.home -> {
+                super.onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+}
