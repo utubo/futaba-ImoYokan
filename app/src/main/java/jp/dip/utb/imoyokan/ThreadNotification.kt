8eb0fe600a58b72691965395a51eabfb117b3d48
utubo <utbutb@gmail.com>
2019-11-10 21:21:13 +0900

    カタログのソート順を保持するように修正

--- app/src/main/java/jp/dip/utb/imoyokan/ThreadNotification.kt
+++ app/src/main/java/jp/dip/utb/imoyokan/ThreadNotification.kt
@@ -62,8 +62,10 @@ class ThreadNotification(private val context: Context, private val intent: Inten
 
     private fun notifyAsync(threadInfo: ThreadInfo, title: String, text: String) {
 
-        // 「カタログでmayを開く→imgのスレを共有で開く→カタログボタンをタップ」したときもimgのカタログを表示するように頑張る
-        Pref.getInstance(context).lastCatalogUrl = getCatalogUrl(threadInfo.url)
+        // 「mayのカタログを開く→imgのスレを共有で開く→カタログボタンをタップ」したときもimgのカタログを表示するように頑張る
+        val pref = Pref.getInstance(context)
+        val (_, _, sort) = analyseCatalogUrl(pref.lastCatalogUrl) ?: Triple("", "", "") // ソート順は引き継ぐ
+        pref.lastCatalogUrl = getCatalogUrl(threadInfo.url, sort)
 
         // フォームデータを上書き
         intent.putExtra(KEY_EXTRA_MAIL, threadInfo.form.mail)
