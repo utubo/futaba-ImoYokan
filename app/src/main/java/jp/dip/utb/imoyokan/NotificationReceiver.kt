e59f20fcbae5339425105c816ec28f2d2dc7e62f
utubo <utbutb@gmail.com>
2019-12-10 10:13:20 +0900

    ソース整理

--- app/src/main/java/jp/dip/utb/imoyokan/NotificationReceiver.kt
+++ app/src/main/java/jp/dip/utb/imoyokan/NotificationReceiver.kt
@@ -43,59 +43,68 @@ class NotificationReceiver : BroadcastReceiver() {
 
         // 返信
         val isConfirmed = intent.hasExtra(KEY_EXTRA_CONFIRMED)
-        val inputText = if (isConfirmed) {
-            intent.getStringExtra(KEY_EXTRA_REPLY_TEXT) ?: ""
-        } else {
-            val remoteInput = RemoteInput.getResultsFromIntent(intent)
-            remoteInput.getString(KEY_EXTRA_REPLY_TEXT, "")
+        val inputText = when {
+            isConfirmed -> intent.getStringExtra(KEY_EXTRA_REPLY_TEXT) ?: ""
+            else -> RemoteInput.getResultsFromIntent(intent).getString(KEY_EXTRA_REPLY_TEXT, "")
         }
 
         // メールアドレスを設定する
-        val defaultMail = intent.str(KEY_EXTRA_MAIL) // 書き込むときは基本的にintentにある(通知に表示中)のメアドが正義
+        val defaultMail = intent.str(KEY_EXTRA_MAIL) // 基本的にintentにある(通知に表示中)のメアドが正義(prefのメアドは使わない)
         var (mail, text) = pickMailAndText(defaultMail, inputText, pref) // ただし入力で上書きされることがある
         if (mail != defaultMail || text.isNotBlank()) {
             pref.mail.set(mail, url)
             pref.apply()
         }
-        if (text.isEmpty()) {
+        if (text.isBlank()) {
             // 本文がないときはメールアドレス設定の結果を表示するスペースがある
-            if (defaultMail == mail) {
-                ThreadNotification(context, intent).notifyCache("本文が無いよ")
-            } else if (mail.isBlank()) {
-                ThreadNotification(context, intent).notifyCache("メールアドレスをクリアしました")
-            } else {
-                ThreadNotification(context, intent).notifyCache("メールアドレスをセットしました", mail)
+            when {
+                defaultMail == mail -> {
+                    ThreadNotification(context, intent).notifyCache("本文が無いよ")
+                }
+                mail.isBlank() -> {
+                    ThreadNotification(context, intent).notifyCache("メールアドレスをクリアしました")
+                }
+                else -> {
+                    ThreadNotification(context, intent).notifyCache("メールアドレスをセットしました", mail)
+                }
             }
             return
         }
 
         // 本文があるなら返信するよ
         text = text.addLineBreakForSingleLineInput()
-        GlobalScope.launch {
-            if (pref.confirmBeforeReply && !isConfirmed) {
+        when {
+            pref.confirmBeforeReply && !isConfirmed -> {
                 ReplyConfirmNotification(context, intent).notifyThis(url, intent.str(KEY_EXTRA_PTUA), mail, inputText, text)
-            } else if (pref.debugMode) {
+            }
+            pref.debugMode -> {
                 ThreadNotification(context, intent).notify("Debug - 返信キャンセル", "mail=${mail},text=${text}")
-            } else {
-                val (title, msg) = Replier().reply(url, text, mail, intent.str(KEY_EXTRA_PTUA))
-                ThreadNotification(context, intent).notify(title, msg)
+            }
+            else -> {
+                GlobalScope.launch {
+                    val (title, msg) = Replier().reply(url, text, mail, intent.str(KEY_EXTRA_PTUA))
+                    ThreadNotification(context, intent).notify(title, msg)
+                }
             }
         }
     }
 
     private fun pickMailAndText(defaultMail: String, text: String, pref: Pref): Pair<String, String> {
-        val regex = if (pref.mail.ignoreWideChar) {
-            """^[@＠]([^ 　]*)[ 　]+(.*)""".toRegex()
-        } else {
-            """^@([^ ]*) +(.*)""".toRegex()
+        val regex = when {
+            pref.mail.ignoreWideChar -> """^[@＠]([^ 　]*)[ 　]+(.*)""".toRegex()
+            else -> """^@([^ ]*) +(.*)""".toRegex()
         }
         val m = regex.find(text)
-        return if (m != null) {
-            m.groupValues[1] to m.groupValues[2].trim()
-        } else if (text == "@" || pref.mail.ignoreWideChar && text == "＠") {
-            (if (defaultMail.isBlank()) pref.mail.lastNoBlank else "") to ""
-        } else {
-            defaultMail to text
+        return when {
+            m != null -> {
+                m.groupValues[1] to m.groupValues[2].trim()
+            }
+            text == "@" || pref.mail.ignoreWideChar && text == "＠" -> {
+                (if (defaultMail.isBlank()) pref.mail.lastNoBlank else "") to ""
+            }
+            else -> {
+                defaultMail to text
+            }
         }
     }
 
